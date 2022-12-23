7f272999e061b5f71db71542563e8e49f7216e72
Noah Durell <noah.durell@klaviyo.com>
2022-12-23 18:55:16 -0500

    try another initializer

--- Sources/KlaviyoSwift/Vendor/ComposableArchitecture/Store.swift
+++ Sources/KlaviyoSwift/Vendor/ComposableArchitecture/Store.swift
@@ -541,7 +541,7 @@ typealias StoreOf<R: ReducerProtocol> = Store<R.State, R.Action>
       var isSending = false
       let rescopedStore = Store<RescopedState, RescopedAction>(
         initialState: toRescopedState(scopedStore.state.value),
-        reducer: .init { rescopedState, rescopedAction in
+        reducer: ReducerProtocol(reduce: { rescopedState, rescopedAction in
           isSending = true
           defer { isSending = false }
           let task = self.root.send(fromScopedAction(fromRescopedAction(rescopedAction)))
@@ -551,7 +551,7 @@ typealias StoreOf<R: ReducerProtocol> = Store<R.State, R.Action>
           } else {
             return .none
           }
-        }
+        })
       )
       rescopedStore.parentCancellable = scopedStore.state
         .dropFirst()
