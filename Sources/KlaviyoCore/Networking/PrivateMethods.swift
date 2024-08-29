ae86e6948dbc62ccdb2f0c7bc5bad72eaab127ed
Ajay Subramanya <118314354+ajaysubra@users.noreply.github.com>
2024-08-29 16:14:16 -0500

    better comments

--- Sources/KlaviyoCore/Networking/PrivateMethods.swift
+++ Sources/KlaviyoCore/Networking/PrivateMethods.swift
@@ -12,6 +12,7 @@ import Foundation
 /// - Parameter name: The name of the SDK, defaults to swift but react native will pass it's own name. DO NOT override this in your apps as our backend will not accept unsupported values here and your network requests will fail.
 /// - Parameter version: The version of the swift SDK default to hard coded values here but react native will pass it's own values here. DO NOT override this in your apps.
 @_spi(KlaviyoPrivate)
+@available(*, deprecated, message: "This function is for internal use only, and should NOT be used in production applications")
 public func overrideSDKDefaults(url: String? = nil, name: String? = nil, version: String? = nil) {
     if let url = url {
         environment.apiURL = { url }
