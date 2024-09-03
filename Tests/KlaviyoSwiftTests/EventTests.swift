41fbf802141905c3749a73c81c8dbb84fd956c55
Andrew Balmer <andrew.balmer@klaviyo.com>
2024-09-03 09:11:26 -0500

    added unit test

--- Tests/KlaviyoSwiftTests/EventTests.swift
+++ Tests/KlaviyoSwiftTests/EventTests.swift
@@ -5,4 +5,13 @@
 //  Created by Andrew Balmer on 9/3/24.
 //
 
+@testable import KlaviyoSwift
 import Foundation
+import XCTest
+
+class KlaviyoEventTests: XCTestCase {
+    func testOpenedPushEvent() {
+        let openedPushEvent = Event.EventName._openedPush
+        XCTAssertEqual(openedPushEvent, .CustomEvent("_openedPush"))
+    }
+}
