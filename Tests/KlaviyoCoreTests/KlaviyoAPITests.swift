62a51c4beeeb6f70203b8930220c917507edc227
Ajay Subramanya <118314354+ajaysubra@users.noreply.github.com>
2024-09-03 22:08:35 -0500

    excluding core snapshots

--- Tests/KlaviyoCoreTests/KlaviyoAPITests.swift
+++ Tests/KlaviyoCoreTests/KlaviyoAPITests.swift
@@ -12,8 +12,6 @@ import XCTest
 @MainActor
 final class KlaviyoAPITests: XCTestCase {
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
         environment = KlaviyoEnvironment.test()
     }
 
