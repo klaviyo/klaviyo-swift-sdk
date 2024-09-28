56bc9242f817596d49b8ed47ce748d35d89ee3ea
Andrew Balmer <andrew.balmer@klaviyo.com>
2024-09-28 19:08:17 -0400

    initial KlaviyoWebViewController implementation
    removed config parameter
    
    added `createWebView` comments

--- Sources/KlaviyoUI/KlaviyoWebViewController.swift
+++ Sources/KlaviyoUI/KlaviyoWebViewController.swift
@@ -4,3 +4,25 @@
 //
 //  Created by Andrew Balmer on 9/28/24.
 //
+
+import UIKit
+import WebKit
+
+class KlaviyoWebViewController: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
+
+    override func loadView() {
+        super.loadView()
+
+        webView = createWebView()
+
+        view.addSubview(webView)
+    }
+
+    func createWebView() -> WKWebView {
+        let webView = WKWebView()
+        // customize any WKWebView behaviors here
+        // ex: webView.allowsBackForwardNavigationGestures = true
+        return webView
+    }
+}
