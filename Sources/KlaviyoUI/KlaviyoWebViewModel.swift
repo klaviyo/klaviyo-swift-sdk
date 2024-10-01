b4e1e9354059e641ee3ca8f720ac654fd09e2a19
Andrew Balmer <andrew.balmer@klaviyo.com>
2024-09-30 23:20:31 -0400

    added KlaviyoWebViewModel

---
+++ Sources/KlaviyoUI/KlaviyoWebViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  KlaviyoWebViewModel.swift
+//  klaviyo-swift-sdk
+//
+//  Created by Andrew Balmer on 9/30/24.
+//
+
+import Foundation
+
+class KlaviyoWebViewModel {
+    let url: URL
+
+    init(url: URL) {
+        self.url = url
+    }
+}
