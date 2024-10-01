91dc2531a4814996b1dfde688cbfcdf979f58fc9
Andrew Balmer <andrew.balmer@klaviyo.com>
2024-09-30 20:14:25 -0400

    added `FileIO.getFileUrl(...)` method

--- Sources/KlaviyoUI/Utilities/FileIO.swift
+++ Sources/KlaviyoUI/Utilities/FileIO.swift
@@ -12,6 +12,14 @@ enum FileIOError: Error {
 }
 
 enum FileIO {
+    static func getFileUrl(path: String, type: String) throws -> URL {
+        guard let fileUrl = Bundle.module.url(forResource: path, withExtension: type) else {
+            throw FileIOError.notFound
+        }
+
+        return fileUrl
+    }
+
     static func getFileContents(path: String, type: String) throws -> String {
         guard let path = Bundle.module.path(forResource: path, ofType: type) else {
             throw FileIOError.notFound
