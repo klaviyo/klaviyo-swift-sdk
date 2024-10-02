2b7f8fbad25a41c8518db8fbcccb8b000303988e
Andrew Balmer <andrew.balmer@klaviyo.com>
2024-10-01 21:57:53 -0400

    implement load script and script message handler
    revised ViewModel

--- Sources/KlaviyoUI/KlaviyoWebView/KlaviyoWebViewModel.swift
+++ Sources/KlaviyoUI/KlaviyoWebView/KlaviyoWebViewModel.swift
@@ -24,7 +24,10 @@ class KlaviyoWebViewModel: KlaviyoWebViewModeling {
     private static func initializeLoadScripts() -> [String: WKUserScript] {
         var scripts: [String: WKUserScript] = [:]
 
-        // TODO: initialize scripts
+        if let toggleHandlerScript = try? FileIO.getFileContents(path: "toggleHandler", type: "js") {
+            let script = WKUserScript(source: toggleHandlerScript, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+            scripts["toggleMessageHandler"] = script
+        }
 
         return scripts
     }
@@ -36,6 +39,31 @@ class KlaviyoWebViewModel: KlaviyoWebViewModeling {
     }
 
     func handleScriptMessage(_ message: WKScriptMessage) {
-        // TODO: handle script message
+        if message.name == "toggleMessageHandler" {
+            guard let dict = message.body as? [String: AnyObject] else {
+                return
+            }
+
+            guard let toggleEnabled = dict["toggleEnabled"] as? Bool else {
+                return
+            }
+
+            print("toggle enabled: \(toggleEnabled)")
+
+            let newTitle = toggleEnabled ? "Toggle is on" : "Toggle is off"
+
+            let script = "document.getElementById('title').innerText = \"\(newTitle)\""
+
+            scriptSubject.send((script, { result in
+                switch result {
+                case let .success(content):
+                    if let successMessage = content as? String {
+                        print("Successfully evaluated Javascript; message: \(successMessage)")
+                    }
+                case let .failure(failure):
+                    print("Javascript evaluation failed; message: \(failure.localizedDescription)")
+                }
+            }))
+        }
     }
 }
