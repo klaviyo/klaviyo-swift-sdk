dcecb1179aea53194cf923dbcfa8f1d57827fa3c
Andrew Balmer <andrew.balmer@klaviyo.com>
2024-10-07 11:03:33 -0500

    added placeholder files

---
+++ Sources/KlaviyoGeo/KlaviyoGeo.swift
@@ -0,0 +1,16 @@
+//
+//  KlaviyoGeo.swift
+//  klaviyo-swift-sdk
+//
+//  Created by Andrew Balmer on 10/7/24.
+//
+
+import Foundation
+
+public struct KlaviyoGeoPlaceholder {
+    public init() {}
+
+    public func helloWorld() {
+        print("Hello world!")
+    }
+}
