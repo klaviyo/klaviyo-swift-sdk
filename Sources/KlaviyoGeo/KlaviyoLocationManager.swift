40061a313d0edc62d4f51f65190424faaef0bfb7
Andrew Balmer <andrew.balmer@klaviyo.com>
2024-10-08 15:45:15 -0400

    added KlaviyoLocationManager

---
+++ Sources/KlaviyoGeo/KlaviyoLocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  KlaviyoLocationManager.swift
+//  klaviyo-swift-sdk
+//
+//  Created by Andrew Balmer on 10/7/24.
+//
+
+import CoreLocation
+import Foundation
+
+public class KlaviyoLocationManager {
+    // TODO: implement this
+}
