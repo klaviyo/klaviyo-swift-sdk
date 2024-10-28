0a425856fbf285cadbbe96fadc429c522da5045d
Andrew Balmer <andrew.balmer@klaviyo.com>
2024-10-28 12:29:13 -0400

    added `default` value to `ShadowStyle`

--- Sources/KlaviyoUI/Styles/ShadowStyle.swift
+++ Sources/KlaviyoUI/Styles/ShadowStyle.swift
@@ -13,3 +13,11 @@ public struct ShadowStyle {
     let offset: CGSize
     let radius: CGFloat
 }
+
+extension ShadowStyle {
+    public static var `default`: Self = .init(
+        color: UIColor.black.cgColor,
+        opacity: 0.5,
+        offset: CGSize(width: 5, height: 5),
+        radius: 8)
+}
