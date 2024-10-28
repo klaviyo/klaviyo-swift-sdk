8df9e62404ef1c669b3ec1a19b2960cc2182b8b7
Andrew Balmer <andrew.balmer@klaviyo.com>
2024-10-28 11:03:00 -0400

    added ShadowStyle

---
+++ Sources/KlaviyoUI/Styles/ShadowStyle.swift
@@ -0,0 +1,15 @@
+//
+//  ShadowStyle.swift
+//  klaviyo-swift-sdk
+//
+//  Created by Andrew Balmer on 10/28/24.
+//
+
+import UIKit
+
+public struct ShadowStyle {
+    let color: CGColor
+    let opacity: Float
+    let offset: CGSize
+    let radius: CGFloat
+}
