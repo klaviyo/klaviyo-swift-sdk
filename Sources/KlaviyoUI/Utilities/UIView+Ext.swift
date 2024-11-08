88b9c09fb575ad8e52f619e53bcb64d3dbcc61ba
Andrew Balmer <andrew.balmer@klaviyo.com>
2024-11-08 11:07:18 -0300

    added `pin(...)` method to UIView+Ext

--- Sources/KlaviyoUI/Utilities/UIView+Ext.swift
+++ Sources/KlaviyoUI/Utilities/UIView+Ext.swift
@@ -5,6 +5,7 @@
 //  Created by Andrew Balmer on 10/25/24.
 //
 
+import SwiftUI
 import UIKit
 
 extension UIView {
@@ -32,5 +33,48 @@ extension UIView {
             bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -insets.bottom),
             trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -insets.trailing)
         ])
+    }
+
+    func pin(to safeArea: UILayoutGuide, edges: [EdgeInset]) {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        var topConstraint: NSLayoutConstraint? {
+            guard let edge = edges.last(where: { $0.containsTop }) else { return nil }
+            return generateConstraint(for: topAnchor, equalTo: safeArea.topAnchor, constant: edge.constant)
+        }
+
+        var leadingConstraint: NSLayoutConstraint? {
+            guard let edge = edges.last(where: { $0.containsLeading }) else { return nil }
+            return generateConstraint(for: leadingAnchor, equalTo: safeArea.leadingAnchor, constant: edge.constant)
+        }
+
+        var bottomConstraint: NSLayoutConstraint? {
+            guard let edge = edges.last(where: { $0.containsBottom }) else { return nil }
+            let constant = edge.constant != nil ? -edge.constant! : nil
+            return generateConstraint(for: bottomAnchor, equalTo: safeArea.bottomAnchor, constant: constant)
+        }
+
+        var trailingConstraint: NSLayoutConstraint? {
+            guard let edge = edges.last(where: { $0.containsTrailing }) else { return nil }
+            let constant = edge.constant != nil ? -edge.constant! : nil
+            return generateConstraint(for: trailingAnchor, equalTo: safeArea.trailingAnchor, constant: constant)
+        }
+
+        let constraints = [
+            topConstraint,
+            leadingConstraint,
+            bottomConstraint,
+            trailingConstraint
+        ].compactMap { $0 }
+
+        NSLayoutConstraint.activate(constraints)
+    }
+}
+
+private func generateConstraint<T>(for from: NSLayoutAnchor<T>, equalTo: NSLayoutAnchor<T>, constant: CGFloat? = nil) -> NSLayoutConstraint {
+    if let constant {
+        from.constraint(equalTo: equalTo, constant: constant)
+    } else {
+        from.constraint(equalTo: equalTo)
     }
 }
