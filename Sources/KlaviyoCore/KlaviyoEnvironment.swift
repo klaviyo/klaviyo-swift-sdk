7ba6dc86674939fe1a9ee9a9a25ce1ce5f01ed12
Daniel Peluso <daniel.peluso@klaviyo.com>
2024-11-12 16:09:30 -0500

    updating name of plist file and fields

--- Sources/KlaviyoCore/KlaviyoEnvironment.swift
+++ Sources/KlaviyoCore/KlaviyoEnvironment.swift
@@ -124,14 +124,14 @@ public struct KlaviyoEnvironment {
     private static let rnSDKConfig: [String: AnyObject] = loadPlist(named: "klaviyo-sdk-configuration") ?? [:]
 
     private static func getSDKName() -> String {
-        if let sdkName = KlaviyoEnvironment.rnSDKConfig["react_native_sdk_name"] as? String {
+        if let sdkName = KlaviyoEnvironment.rnSDKConfig["klaviyo_sdk_name"] as? String {
             return sdkName
         }
         return __klaviyoSwiftName
     }
 
     private static func getSDKVersion() -> String {
-        if let sdkVersion = KlaviyoEnvironment.rnSDKConfig["react_native_sdk_version"] as? String {
+        if let sdkVersion = KlaviyoEnvironment.rnSDKConfig["klaviyo_sdk_version"] as? String {
             return sdkVersion
         }
         return __klaviyoSwiftVersion
