d134ee3641ab7c26df874b1bf5c720e6919499be
Andrew Balmer <andrew.balmer@klaviyo.com>
2024-11-27 17:27:28 -0500

    added stub structs to `FullFormsResponse`

--- Sources/KlaviyoCore/Models/APIModels/FullFormsResponse.swift
+++ Sources/KlaviyoCore/Models/APIModels/FullFormsResponse.swift
@@ -7,42 +7,32 @@
 
 import Foundation
 
-public struct FullFormsResponse: Equatable {
-    public let forms: [Data]
-    public let formSettings: Data
-    public let dynamicInfoConfig: Data?
+public struct FullFormsResponse: Codable, Equatable {
+    public let fullForms: [FullForm]
+    public let formSettings: FormSettings
+    public let dynamicInfoConfig: DynamicInfoConfig?
 
-    private enum CodingKeys: String, CodingKey {
-        case forms = "full_forms"
+    enum CodingKeys: String, CodingKey {
+        case fullForms = "full_forms"
         case formSettings = "form_settings"
         case dynamicInfoConfig = "dynamic_info_config"
     }
+}
 
-    public init(data: Data) throws {
-        guard JSONSerialization.isValidJSONObject(data) else {
-            throw KlaviyoDecodingError.cannotConvertToJson
-        }
+extension FullFormsResponse {
+    public struct FullForm: Codable, Equatable {
+        // TODO: determine which properties we need to decode
+    }
+}
 
-        guard let jsonObject = try JSONSerialization.jsonObject(with: data) as? [String: Any] else {
-            throw KlaviyoDecodingError.invalidType
-        }
+extension FullFormsResponse {
+    public struct FormSettings: Codable, Equatable {
+        // TODO: determine which properties we need to decode
+    }
+}
 
-        let fullFormsObject: [[String: Any]] = try jsonObject.value(forKey: CodingKeys.forms.rawValue)
-        forms = try fullFormsObject.map {
-            let data = try JSONSerialization.data(withJSONObject: $0)
-            return data
-        }
-
-        let formSettingsObject: [String: Any] = try jsonObject.value(forKey: CodingKeys.formSettings.rawValue)
-        let formSettingsData = try JSONSerialization.data(withJSONObject: formSettingsObject)
-        formSettings = formSettingsData
-
-        let dynamicInfoConfigObject: [String: Any] = try jsonObject.value(forKey: CodingKeys.dynamicInfoConfig.rawValue)
-        if !dynamicInfoConfigObject.isEmpty {
-            let dynamicInfoConfigData = try JSONSerialization.data(withJSONObject: dynamicInfoConfigObject)
-            dynamicInfoConfig = dynamicInfoConfigData
-        } else {
-            dynamicInfoConfig = nil
-        }
+extension FullFormsResponse {
+    public struct DynamicInfoConfig: Codable, Equatable {
+        // TODO: determine which properties we need to decode
     }
 }
