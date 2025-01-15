250837fae7c26d556281a96e74d28f070169e86a
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-01-15 17:58:03 -0500

    removed `scriptStream` from KlaviyoWebViewModeling
    # Conflicts:
    #	Sources/KlaviyoUI/KlaviyoWebView/KlaviyoWebViewModeling.swift

--- Sources/KlaviyoUI/KlaviyoWebView/KlaviyoWebViewModeling.swift
+++ Sources/KlaviyoUI/KlaviyoWebView/KlaviyoWebViewModeling.swift
@@ -16,9 +16,6 @@ protocol KlaviyoWebViewModeling {
     /// Scripts to be injected into the ``WKWebView`` when the website loads.
     var loadScripts: [String: WKUserScript]? { get }
 
-    /// Streams scripts for the ``WKWebView`` to execute.
-    var scriptStream: AsyncStream<(script: String, callback: ((Result<Any?, Error>) -> Void)?)> { get }
-
     func handleNavigationEvent(_ event: WKNavigationEvent)
     func handleScriptMessage(_ message: WKScriptMessage)
 }
