81d4ee62b4caf9ded2ff1bc9b083cc6e5858376e
Ajay Subramanya <118314354+ajaysubra@users.noreply.github.com>
2025-01-24 17:34:26 -0600

    removing some random code

--- Sources/KlaviyoUI/KlaviyoUI.swift
+++ Sources/KlaviyoUI/KlaviyoUI.swift
@@ -12,52 +12,9 @@ struct KlaviyoUIState {
 }
 
 public struct KlaviyoUI {
-    private static var state = KlaviyoUIState(companyId: nil)
-    private static let fileName = "companyId.txt"
-
-    
-    public init() {
-        if KlaviyoUI.state.companyId == nil {
-            KlaviyoUI.state.companyId = KlaviyoUI.loadCompanyId()
-        }
-    }
-    public func initilize(companyId: String) {
-        KlaviyoUI.state.companyId = companyId
-        KlaviyoUI.saveCompanyIdToFile(companyId)
+    func helloWorld() {
+        print("Hello world!")
     }
 }
-
-
-extension KlaviyoUI {
-    
-    // MARK: - Private File Persistence Methods
-
-    private static func getDocumentsDirectory() -> URL {
-        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-    }
-
-    private static func saveCompanyIdToFile(_ companyId: String) {
-        let fileURL = getDocumentsDirectory().appendingPathComponent(fileName)
-        do {
-            try companyId.write(to: fileURL, atomically: true, encoding: .utf8)
-            print("Company ID saved to file.")
-        } catch {
-            print("Failed to save company ID: \(error)")
-        }
-    }
-
-    private static func loadCompanyId() -> String? {
-        let fileURL = getDocumentsDirectory().appendingPathComponent(fileName)
-        do {
-            let savedCompanyId = try String(contentsOf: fileURL, encoding: .utf8)
-            print("Loaded Company ID from file.")
-            return savedCompanyId
-        } catch {
-            print("No company ID found in file or failed to load: \(error)")
-            return nil
-        }
-    }
-}
-
 
 
