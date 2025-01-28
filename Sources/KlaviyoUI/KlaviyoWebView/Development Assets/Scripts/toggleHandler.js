a552d0a65b03847a31a7bd80b9c3e3a30c301db6
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-01-28 12:15:00 -0500

    added console.log messages to toggleHandler and closeHandler

--- Sources/KlaviyoUI/KlaviyoWebView/Development Assets/Scripts/toggleHandler.js
+++ Sources/KlaviyoUI/KlaviyoWebView/Development Assets/Scripts/toggleHandler.js
@@ -1,6 +1,7 @@
 var _selector = document.querySelector('input[name=myCheckbox]');
 _selector.addEventListener('change', function(event) {
   if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.toggleMessageHandler) {
+    console.log("toggle changed; new value: " + _selector.checked);
     window.webkit.messageHandlers.toggleMessageHandler.postMessage({
       "toggleEnabled": _selector.checked
     });
