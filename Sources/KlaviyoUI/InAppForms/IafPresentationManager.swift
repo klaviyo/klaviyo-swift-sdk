55e3b74bad5e9a5927710c09edcfe7045c157e5d
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-02-03 16:42:56 -0500

    added IAFPresentationManager

---
+++ Sources/KlaviyoUI/InAppForms/IafPresentationManager.swift
@@ -0,0 +1,57 @@
+//
+//  IafPresentationManager.swift
+//  klaviyo-swift-sdk
+//
+//  Created by Andrew Balmer on 2/3/25.
+//
+
+import Foundation
+import OSLog
+import UIKit
+
+class IafPresentationManager {
+    static let shared = IafPresentationManager()
+
+    lazy var indexHtmlFileUrl: URL? = {
+        do {
+            return try ResourceLoader.getResourceUrl(path: "InAppFormsTemplate", type: "html")
+        } catch {
+            return nil
+        }
+    }()
+
+    private var isLoading: Bool = false
+
+    @MainActor func presentIaf() {
+        guard !isLoading else {
+            if #available(iOS 14.0, *) {
+                Logger.webViewLogger.log("In-App Form is already loading; ignoring request.")
+            }
+            return
+        }
+
+        guard let fileUrl = indexHtmlFileUrl else {
+            if #available(iOS 14.0, *) {
+                Logger.webViewLogger.warning("URL for local HTML file is nil; unable to present In-App Form.")
+            }
+            return
+        }
+
+        isLoading = true
+
+        let viewModel = IafWebViewModel(url: fileUrl)
+        let viewController = KlaviyoWebViewController(viewModel: viewModel)
+        viewController.modalPresentationStyle = .overCurrentContext
+
+        Task {
+            defer { isLoading = false }
+
+            try await viewModel.preloadWebsite(timeout: 8_000_000_000)
+
+            guard let topController = UIApplication.shared.topMostViewController else {
+                return
+            }
+            topController.present(viewController, animated: true, completion: nil)
+        }
+    }
+}
