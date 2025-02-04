c98f4bfad150bfdfd367350c166eb084c8cb849f
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-02-04 17:02:22 -0500

    added KlaviyoInternal struct

---
+++ Sources/KlaviyoSwift/KlaviyoInternal.swift
@@ -0,0 +1,11 @@
+//
+//  KlaviyoSDK.swift
+//  klaviyo-swift-sdk
+//
+//  Created by Andrew Balmer on 2/4/25.
+//
+
+import KlaviyoCore
+
+/// The internal interface for the Klaviyo SDK. Can only be accessed from other modules within the Klaviyo-Swift-SDK package; cannot be accessed from the host app.
+package struct KlaviyoInternal {}
