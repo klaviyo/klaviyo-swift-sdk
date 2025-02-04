23d84a959ff3f71b20a3413610cb13c9a703cd2a
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-02-04 17:03:00 -0500

    added `create` method to KlaviyoInternal

--- Sources/KlaviyoSwift/KlaviyoInternal.swift
+++ Sources/KlaviyoSwift/KlaviyoInternal.swift
@@ -8,4 +8,10 @@
 import KlaviyoCore
 
 /// The internal interface for the Klaviyo SDK. Can only be accessed from other modules within the Klaviyo-Swift-SDK package; cannot be accessed from the host app.
-package struct KlaviyoInternal {}
+package struct KlaviyoInternal {
+    /// Create and send an aggregate event.
+    /// - Parameter event: the event to be tracked in Klaviyo
+    package static func create(aggregateEvent: AggregateEventPayload) {
+        dispatchOnMainThread(action: .enqueueAggregateEvent(aggregateEvent))
+    }
+}
