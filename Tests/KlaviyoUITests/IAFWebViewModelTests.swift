666ab279ca051eb124de000fa3d1cab4cf1371bb
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-02-06 15:46:51 -0500

    added test setup

--- Tests/KlaviyoUITests/IAFWebViewModelTests.swift
+++ Tests/KlaviyoUITests/IAFWebViewModelTests.swift
@@ -10,4 +10,28 @@ import KlaviyoCore
 import WebKit
 import XCTest
 
-final class IAFWebViewModelTests: XCTestCase {}
+final class IAFWebViewModelTests: XCTestCase {
+    // MARK: - setup
+
+    var viewModel: IAFWebViewModel!
+    var viewController: KlaviyoWebViewController!
+
+    override func setUpWithError() throws {
+        super.setUp()
+
+        environment.sdkName = { "swift" }
+        environment.sdkVersion = { "0.0.1" }
+
+        let fileUrl = try ResourceLoader.getResourceUrl(path: "IAFUnitTest", type: "html")
+
+        viewModel = IAFWebViewModel(url: fileUrl)
+        viewController = KlaviyoWebViewController(viewModel: viewModel)
+    }
+
+    override func tearDown() {
+        viewModel = nil
+        viewController = nil
+
+        super.tearDown()
+    }
+}
