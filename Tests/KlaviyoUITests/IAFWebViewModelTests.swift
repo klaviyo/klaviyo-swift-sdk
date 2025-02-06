49c8e8cb717c3dc4a675251e23d29d31f0493659
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-02-06 15:45:58 -0500

    added IAFWebViewModelTests class

---
+++ Tests/KlaviyoUITests/IAFWebViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  IAFWebViewModelTests.swift
+//  klaviyo-swift-sdk
+//
+//  Created by Andrew Balmer on 2/6/25.
+//
+
+@testable @_spi(KlaviyoPrivate) import KlaviyoUI
+import KlaviyoCore
+import WebKit
+import XCTest
+
+final class IAFWebViewModelTests: XCTestCase {}
