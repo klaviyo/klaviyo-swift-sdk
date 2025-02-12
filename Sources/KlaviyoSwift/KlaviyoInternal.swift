1ee297c7e4ad0f3fa50c0a4e53e96f72c099d4b5
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-02-12 15:59:20 -0500

    modified documentation

--- Sources/KlaviyoSwift/KlaviyoInternal.swift
+++ Sources/KlaviyoSwift/KlaviyoInternal.swift
@@ -7,7 +7,9 @@
 
 import KlaviyoCore
 
-/// The internal interface for the Klaviyo SDK. Can only be accessed from other modules within the Klaviyo-Swift-SDK package; cannot be accessed from the host app.
+/// The internal interface for the Klaviyo SDK.
+///
+/// - Note: Can only be accessed from other modules within the Klaviyo-Swift-SDK package; cannot be accessed from the host app.
 package struct KlaviyoInternal {
     /// Create and send an aggregate event.
     /// - Parameter event: the event to be tracked in Klaviyo
