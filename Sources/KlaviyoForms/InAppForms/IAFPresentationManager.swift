105275a538e30c15102d416cc6b85c49d56eed6d
Evan Masseau <>
2025-03-05 16:43:00 -0500

    Also need to fix dismissing after timeouts

--- Sources/KlaviyoForms/InAppForms/IAFPresentationManager.swift
+++ Sources/KlaviyoForms/InAppForms/IAFPresentationManager.swift
@@ -56,6 +56,7 @@ class IAFPresentationManager {
             do {
                 try await viewModel.preloadWebsite(timeout: NetworkSession.networkTimeout)
             } catch {
+                await viewController.dismiss()
                 if #available(iOS 14.0, *) {
                     Logger.webViewLogger.warning("Error preloading In-App Form: \(error).")
                 }
@@ -63,10 +64,12 @@ class IAFPresentationManager {
             }
 
             guard let topController = UIApplication.shared.topMostViewController else {
+                await viewController.dismiss()
                 return
             }
 
             if topController.isKlaviyoVC || topController.hasKlaviyoVCInStack {
+                await viewController.dismiss()
                 if #available(iOS 14.0, *) {
                     Logger.webViewLogger.warning("In-App Form is already being presented; ignoring request")
                 }
