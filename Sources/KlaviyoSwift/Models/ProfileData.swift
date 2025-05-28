5cd0e9c3fb1ff7bea47282b5902d60127a429a9d
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-05-28 11:39:37 +0200

    added `ProfileData.toHtmlString()` and unit tests

--- Sources/KlaviyoSwift/Models/ProfileData.swift
+++ Sources/KlaviyoSwift/Models/ProfileData.swift
@@ -5,6 +5,9 @@
 //  Created by Andrew Balmer on 4/21/25.
 //
 
+import Foundation
+import OSLog
+
 package struct ProfileData: Equatable, CustomDebugStringConvertible {
     package var apiKey: String?
     package var email: String?
@@ -14,11 +17,27 @@ package struct ProfileData: Equatable, CustomDebugStringConvertible {
 
     package var debugDescription: String {
         """
-        apiKey: \t\t\t\(apiKey ?? "<no API key>")
+        apiKey: \t\t\(apiKey ?? "<no API key>")
         email: \t\t\t\(email ?? "<no email>")
         phoneNumber: \t\(phoneNumber ?? "<no phoneNumber>")
         anonymousId: \t\(anonymousId ?? "<no anonymousId>")
         externalId: \t\(externalId ?? "<no externalId>")
         """
+    }
+}
+
+extension ProfileData: Encodable {
+    package func toHtmlString() throws -> String {
+        do {
+            let encoder = JSONEncoder()
+            let jsonData = try encoder.encode(self)
+            let jsonString = String(data: jsonData, encoding: .utf8)
+            return jsonString ?? ""
+        } catch {
+            if #available(iOS 14.0, *) {
+                Logger.codableLogger.warning("Error encoding profile data: \(error)")
+            }
+            return ""
+        }
     }
 }
