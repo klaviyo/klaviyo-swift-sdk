b4dd458f6f38ccd45dcbad4533a93072d8502963
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-06-01 21:06:17 +0100

    renamed `cancellable` to `apiKeyCancellable`

--- Sources/KlaviyoSwift/KlaviyoInternal.swift
+++ Sources/KlaviyoSwift/KlaviyoInternal.swift
@@ -13,12 +13,12 @@ import KlaviyoCore
 ///
 /// - Note: Can only be accessed from other modules within the Klaviyo-Swift-SDK package; cannot be accessed from the host app.
 package enum KlaviyoInternal {
-    static var cancellable: Cancellable?
+    static var apiKeyCancellable: Cancellable?
     /// the apiKey (a.k.a. CompanyID) for the current SDK instance.
     /// - Parameter completion: completion hanlder that will be called when apiKey is avaialble after SDK is initilized
     package static func apiKey(completion: @escaping ((String?) -> Void)) {
-        if cancellable == nil {
-            cancellable = KlaviyoSwiftEnvironment.production.statePublisher()
+        if apiKeyCancellable == nil {
+            apiKeyCancellable = KlaviyoSwiftEnvironment.production.statePublisher()
                 .receive(on: DispatchQueue.main)
                 .filter { $0.initalizationState == .initialized }
                 .compactMap(\.apiKey)
@@ -26,7 +26,7 @@ package enum KlaviyoInternal {
                 .prefix(1)
                 .sink(receiveValue: {
                     completion($0)
-                    cancellable = nil
+                    apiKeyCancellable = nil
                 })
         }
     }
