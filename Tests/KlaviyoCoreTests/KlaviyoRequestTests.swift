30890d8f1456057e051013fb3481dc096553a4fa
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-07-29 09:53:08 -0400

    updated unit tests

--- Tests/KlaviyoCoreTests/KlaviyoRequestTests.swift
+++ Tests/KlaviyoCoreTests/KlaviyoRequestTests.swift
@@ -4,7 +4,8 @@ import XCTest
 final class KlaviyoRequestTests: XCTestCase {
     func testURLRequestSetsAttemptHeader() throws {
         let request = KlaviyoRequest(apiKey: "foo", endpoint: .registerPushToken(.test))
-        let urlRequest = try request.urlRequest(currentAttempt: 3, maxAttempts: 7)
+        let attemptInfo = try RequestAttemptInfo(attemptNumber: 3, maxAttempts: 7)
+        let urlRequest = try request.urlRequest(attemptInfo: attemptInfo)
         let header = urlRequest.value(forHTTPHeaderField: "X-Klaviyo-Attempt-Count")
         XCTAssertEqual(header, "3/7")
     }
