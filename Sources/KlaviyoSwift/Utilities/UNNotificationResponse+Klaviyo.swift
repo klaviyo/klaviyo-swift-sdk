b578a33b0f79f335f2174193a0deaff56d0a68fa
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-08-08 11:09:27 -0400

    refactored logic

--- Sources/KlaviyoSwift/Utilities/UNNotificationResponse+Klaviyo.swift
+++ Sources/KlaviyoSwift/Utilities/UNNotificationResponse+Klaviyo.swift
@@ -19,12 +19,13 @@ extension UNNotificationResponse {
     /// A notification is considered a Klaviyo notification if it contains
     /// a "body" dictionary with a "_k" key in its userInfo.
     public var isKlaviyoNotification: Bool {
-        guard let properties = notification.request.content.userInfo as? [String: Any],
-              let body = properties["body"] as? [String: Any] else {
+        if let properties = notification.request.content.userInfo as? [String: Any],
+           let body = properties["body"] as? [String: Any],
+           let _ = body["_k"] {
+            return true
+        } else {
             return false
         }
-
-        return body["_k"] != nil
     }
 
     /// Returns the custom Klaviyo properties from a Klaviyo notification payload, if present.
