dc6e0cc9393037ea45cace83d0c262fe9e724194
Andrew Balmer <andrew.balmer@klaviyo.com>
2025-09-15 19:19:58 -0500

    added base `UniversalLinkHandler` enum

---
+++ Sources/KlaviyoCore/Utils/UniversalLinkHandler.swift
@@ -0,0 +1,14 @@
+//
+//  UniversalLinkHandler.swift
+//  klaviyo-swift-sdk
+//
+//  Created by Andrew Balmer on 9/15/25.
+//
+
+import OSLog
+import UIKit
+
+@MainActor
+internal enum UniversalLinkHandler {
+    // TODO: implement body
+}
