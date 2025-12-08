b7bf3eb887d302f3bc19f0c8be94f8bd8eb72f5e
Isobelle Lim <isobelle.lim@klaviyo.com>
2025-12-08 10:33:32 -0500

    Cleanup geofencing if permissions were reduced

--- Sources/KlaviyoLocation/KlaviyoLocationManager.swift
+++ Sources/KlaviyoLocation/KlaviyoLocationManager.swift
@@ -40,12 +40,14 @@ class KlaviyoLocationManager: NSObject {
             if #available(iOS 14.0, *) {
                 Logger.geoservices.warning("App does not have 'authorizedAlways' permission to access the user's location")
             }
+            await stopGeofenceMonitoring()
             return
         }
 
         if #available(iOS 14.0, *) {
             guard locationManager.currentAccuracyAuthorization == .fullAccuracy else {
                 Logger.geoservices.warning("App does not have full accuracy permission to access the user's location")
+                await stopGeofenceMonitoring()
                 return
             }
         }
