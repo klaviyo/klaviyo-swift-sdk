4073be0cb2247d90d4a1951a985409f295eb8006
Isobelle Lim <isobelle.lim@klaviyo.com>
2025-12-08 15:35:51 -0500

    Remove monitorGeofencesFromBackground and onGeofenceEvent

--- Sources/KlaviyoLocation/KlaviyoSDK+Location.swift
+++ Sources/KlaviyoLocation/KlaviyoSDK+Location.swift
@@ -17,11 +17,6 @@ extension KlaviyoSDK {
         await KlaviyoLocationManager.shared.startGeofenceMonitoring()
     }
 
-    /// To be called in didFinishLaunchingWithOptions to ensure geofence events that happen in a backgrounded/terminated state are processed.
-    public func monitorGeofencesFromBackground() {
-        KlaviyoLocationManager.shared.monitorGeofencesFromBackground()
-    }
-
     /// Unregisters app for geofencing. Stops monitoring for geofences and cleans up resources.
     @MainActor
     public func unregisterGeofencing() async {
@@ -37,13 +32,5 @@ extension KlaviyoSDK {
     @available(*, deprecated, message: "This function is for internal use only, and should not be used in production applications")
     public func getCurrentGeofences() async -> Set<CLCircularRegion> {
         await Set(KlaviyoLocationManager.shared.getActiveGeofences().map { $0.toCLCircularRegion() })
-    }
-
-    // TODO: add documentation
-    @MainActor
-    @_spi(KlaviyoPrivate)
-    @available(*, deprecated, message: "This function is for internal use only, and should not be used in production applications")
-    public func onGeofenceEvent(_ action: () -> Void) {
-        // TODO: implement handler
     }
 }
