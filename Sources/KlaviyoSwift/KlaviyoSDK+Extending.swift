fd64e96c79a075a549af58123e01948cb73f1470
Isobelle Lim <isobelle.lim@klaviyo.com>
2026-07-14 11:05:02 -0400

    feat(core): add KlaviyoSDKExtending marker protocol + KlaviyoSDK conformance

---
+++ Sources/KlaviyoSwift/KlaviyoSDK+Extending.swift
@@ -0,0 +1,11 @@
+//
+//  KlaviyoSDK+Extending.swift
+//  klaviyo-swift-sdk
+//
+//  Created by Isobelle Lim on 7/14/26.
+//
+
+import Foundation
+import KlaviyoCore
+
+extension KlaviyoSDK: KlaviyoSDKExtending {}
